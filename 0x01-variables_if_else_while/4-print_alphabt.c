856fd89c0543fd56358edd17108e97282740baba
ianotiato <ianotiato688@gmail.com>
2022-10-23 05:11:31 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int m,ain(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar (c);
+	}
+	putchar('\n');
+	return (0);
+}
