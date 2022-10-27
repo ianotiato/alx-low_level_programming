392fee3f1f3eabab516c5c67d985326354a4ee83
ianotiato <ianotiato688@gmail.com>
2022-10-27 04:39:12 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,9 +9,9 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *cursor = h;
-	size_t const = 0;
+	size_t count = 0;
 
-	while (cursor != NULL)
+	wile (cursor != NULL)
 	{
 		printf("%d\n", cursor->n);
 		count += 1;
@@ -19,3 +19,4 @@ size_t print_listint(const listint_t *h)
 	}
 	return (count);
 }
+
