4be4232f5639f153b9ea00d668afb336a8d1529a
ianotiato <ianotiato688@gmail.com>
2022-10-31 11:59:09 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -13,9 +13,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned int m;
 
 	if (index > 63)
-			return (-1);
+		return (-1);
 
-	m = -1 << index;
+	m = 1 << index;
 
 	if (*n & m)
 		*n ^= m;
